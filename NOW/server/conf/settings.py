d3b3db1d6ae2b3f9dcf218f996ddabd2d3572eea
AmberFennek <orkyporky@gmail.com>
2016-04-18 03:12:59 -0400

    Settings changes for new code base + 5h idle time

--- NOW/server/conf/settings.py
+++ NOW/server/conf/settings.py
@@ -1,121 +1,57 @@
 """
 Evennia settings file.
 
-The full options are found in the default settings file found here:
+The available options are found in the default settings file found
+here:
 
-/home/python/evennia/evennia/settings_default.py
+/home/NOW/NOWMU/evennia/evennia/settings_default.py
 
-Note: Don't copy more from the default file than you actually intend to
+Remember:
+
+Don't copy more from the default file than you actually intend to
 change; this will make sure that you don't overload upstream updates
 unnecessarily.
+
+When changing a setting requiring a file system path (like
+path/to/actual/file.py), use GAME_DIR and EVENNIA_DIR to reference
+your game folder and the Evennia library folders respectively. Python
+paths (path.to.module) should be given relative to the game's root
+folder (typeclasses.foo) whereas paths within the Evennia library
+needs to be given explicitly (evennia.foo).
 
 """
 
 # Use the defaults from Evennia unless explicitly overridden
-import os
 from evennia.settings_default import *
 
 ######################################################################
 # Evennia base server config
 ######################################################################
 
-# This is the name of your game. Make it catchy!
 SERVERNAME = "NOW"
 
-# Path to the game directory (use EVENNIA_DIR to refer to the
-# core evennia library)
-GAME_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+TELNET_PORTS = [4000, 8888]
 
-# Place to put log files
-LOG_DIR = os.path.join(GAME_DIR, "server", "logs")
-SERVER_LOG_FILE = os.path.join(LOG_DIR, 'server.log')
-PORTAL_LOG_FILE = os.path.join(LOG_DIR, 'portal.log')
-HTTP_LOG_FILE = os.path.join(LOG_DIR, 'http_requests.log')
+INLINEFUNC_ENABLED = True
 
-# Other defaults
-PROTOTYPE_MODULES = ("world.prototypes",)
+IDLE_TIMEOUT = 18000
 
-INLINEFUNC_ENABLED=True
-LANGUAGE_CODE = 'en-us'
-# How long time (in seconds) a user may idle before being logged
-# out. This can be set as big as desired. A user may avoid being
-# thrown off by sending the empty system command 'idle' to the server
-# at regular intervals. Set <=0 to deactivate idle timeout completely.
-IDLE_TIMEOUT = 9000
-# The idle command can be sent to keep your session active without actually
-# having to spam normal commands regularly. It gives no feedback, only updates
-# the idle timer.
-
-# Actual URL for webclient component to reach the websocket. You only need
-# to set this if you know you need it, like using some sort of proxy setup.
-# If given it must be on the form "ws://hostname" (WEBSOCKET_CLIENT_PORT will
-# be automatically appended). If left at None, the client will itself
-# figure out this url based on the server's hostname.
-# WEBSOCKET_CLIENT_URL = 'ws://localhost'
-
+ENCODINGS = ["utf-8", "latin-1", "ISO-8859-1", "cp437"]
 
 ######################################################################
-# Evennia Database config
+# Evennia modules
 ######################################################################
 
-# Database config syntax:
-# ENGINE - path to the the database backend. Possible choices are:
-#            'django.db.backends.sqlite3', (default)
-#            'django.db.backends.mysql',
-#            'django.db.backends.postgresql_psycopg2',
-#            'django.db.backends.oracle' (untested).
-# NAME - database name, or path to the db file for sqlite3
-# USER - db admin (unused in sqlite3)
-# PASSWORD - db admin password (unused in sqlite3)
-# HOST - empty string is localhost (unused in sqlite3)
-# PORT - empty string defaults to localhost (unused in sqlite3)
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(GAME_DIR, "server", "evennia.db3"),
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': ''
-        }}
+LOCK_FUNC_MODULES = LOCK_FUNC_MODULES + ("world.locks",)
+
 
 ######################################################################
 # Django web features
-# (don't remove these entries, they are needed to override the default
-# locations with your actual GAME_DIR locations at run-time)
 ######################################################################
 
-# Absolute path to the directory that holds file uploads from web apps.
-# Example: "/home/media/media.lawrence.com"
-MEDIA_ROOT = os.path.join(GAME_DIR, "web", "media")
-
-# The master urlconf file that contains all of the sub-branches to the
-# applications. Change this to add your own URLs to the website.
-ROOT_URLCONF = 'web.urls'
-
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure
-# to use a trailing slash. Django1.4+ will look for admin files under
-# STATIC_URL/admin.
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(GAME_DIR, "web", "static")
-
-# Directories from which static files will be gathered from.
-STATICFILES_DIRS = (
-    os.path.join(GAME_DIR, "web", "static_overrides"),
-    os.path.join(EVENNIA_DIR, "web", "static"),)
-
-# Lock functions here!
-LOCK_FUNC_MODULES = LOCK_FUNC_MODULES + ("world.locks",)
-
-# We setup the location of the website template as well as the admin site.
-TEMPLATE_DIRS = (
-    os.path.join(GAME_DIR, "web", "template_overrides", ACTIVE_TEMPLATE),
-    os.path.join(GAME_DIR, "web", "template_overrides"),
-    os.path.join(EVENNIA_DIR, "web", "templates", ACTIVE_TEMPLATE),
-    os.path.join(EVENNIA_DIR, "web", "templates"),)
 
 # The secret key is randomly seeded upon creation. It is used to sign
 # Django's cookies. Do not share this with anyone. Changing it will
 # log out all active web browsing sessions. Game web client sessions
 # may survive.
-SECRET_KEY = 'G%"9"BgqPb./e62y*Zlh1>|@]&HsA),-LwvVf;xS'
+SECRET_KEY = 'qLiEOAy+_Gxc;`eK/)?CQb_H#-p<2T]o|&s64vZR'
