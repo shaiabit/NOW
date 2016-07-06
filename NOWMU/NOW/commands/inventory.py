ba6634d602e58ca4f4d58ad932849491adc3cc30
AmberFennek <orkyporky@gmail.com>
2016-07-06 13:42:31 -0400

    Inventory updated to use EvTable

--- NOWMU/NOW/commands/inventory.py
+++ NOWMU/NOW/commands/inventory.py
@@ -1,6 +1,7 @@
 from evennia import default_cmds
-from evennia.utils import prettytable
+from evennia.utils import evtable
 from world.helpers import mass_unit
+
 
 class CmdInventory(default_cmds.MuxCommand):
     """
@@ -13,6 +14,7 @@ class CmdInventory(default_cmds.MuxCommand):
     key = 'inventory'
     aliases = ['inv', 'i']
     locks = 'cmd:all()'
+    arg_regex = r'^/|\s|$'
 
     def func(self):
         """check inventory"""
@@ -21,18 +23,16 @@ class CmdInventory(default_cmds.MuxCommand):
         if not items:
             string = 'You are not carrying anything.'
         else:
-            table = prettytable.PrettyTable(['name', 'desc_brief'])
-            table.header = False
-            table.border = False
+            table = evtable.EvTable(border='header')
             for item in items:
                 imass = mass_unit(item.get_mass()) if hasattr(item, 'get_mass') else '- unknown -'
-                second = imass if 'weight' in self.switches else item.db.desc_brief or item.db.desc
-                table.add_row(['%s' % item.mxp_name(you.sessions, '@verb #%s' % item.id)
-                               if hasattr(item, 'mxp_name')
-                               else item.get_display_name(you.sessions), second and second or ''])
+                second = '(|y%s|n) ' % imass if 'weight' in self.switches else ''
+                second += item.db.desc_brief or item.db.desc
+                table.add_row('%s' % item.mxp_name(you.sessions, '@verb #%s' % item.id)
+                              if hasattr(item, 'mxp_name')
+                              else item.get_display_name(you.sessions), second and second or '')
             imass = you.get_mass() if hasattr(item, 'get_mass') else '- unknown -'
             string = "|wYou (%s) and what you carry (%s) total |y%s|n:\n%s" % (mass_unit(you.db.mass),
                                                                                mass_unit(imass - you.db.mass),
                                                                                mass_unit(imass), table)
         you.msg(string)
-
