39e488ecfb6ee30d03e3ffdd11138c4148be74b4
AmberFennek <orkyporky@gmail.com>
2016-07-07 12:55:17 -0400

    Restructuring vehicle command set, correct disembark message order

--- NOWMU/NOW/commands/vehicle.py
+++ NOWMU/NOW/commands/vehicle.py
@@ -5,9 +5,7 @@ from evennia import default_cmds
 class VehicleCmdSet(CmdSet):
     def at_cmdset_creation(self):
         """Add command to the set - this set will be attached to the vehicle object (item or room)."""
-        self.add(CmdBoard())
-        self.add(CmdDisembark())
-        self.add(CmdDrive())
+        self.add(CmdVehicle())
 
 
 class CmdVehicleDefault(default_cmds.MuxCommand):
@@ -26,72 +24,38 @@ class CmdVehicleDefault(default_cmds.MuxCommand):
         outside.msg_contents(message)
 
 
-class CmdBoard(CmdVehicleDefault):
+class CmdVehicle(CmdVehicleDefault):
     """
-    Board the vehicle.
+    Operate various aspects of the vehicle as configured.
     Usage:
-      board
+      vehicle  display other commands available.
     """
-    key = 'board'
-    aliases = ['enter']
-
-    def func(self):
-        """ """
-        # cmd = self.cmdstring
-        # args = self.args.strip()
-        char = self.character
-        # here = char.location
-        where = self.obj
-        player = self.player
-        entry_message = where.db.messages['entry']
-        char.msg('%s%s|n %s' % (char.STYLE, char.key, entry_message))
-        player.msg("You board %s." % where.get_display_name(player))
-        char.move_to(where)
-        self.send_msg('%s%s|n %s' % (char.STYLE, char.key, entry_message))
-
-
-class CmdDisembark(CmdVehicleDefault):
-    """
-    Disembark the vehicle.
-    Usage:
-      disembark
-    """
-    key = 'disembark'
-    aliases = ['exit', 'leave']
+    aliases = ['board', 'enter', 'disembark', 'exit', 'leave', 'operate']
 
     def func(self):
         """ """
         cmd = self.cmdstring
-        # args = self.args.strip()
-        char = self.character
-        # here = char.location
-        where = self.obj
-        outside = where.location
-        player = self.player
-
-        player.msg("%s commands: %s" % (char.get_display_name(player), cmd))
-        player.msg("You disembark %s." % where.get_display_name(player))
-        char.move_to(outside)
-
-
-class CmdDrive(CmdVehicleDefault):
-    """
-    Operate the vehicle
-    Usage:
-      drive
-      fly
-    """
-    key = 'drive'
-    aliases = ['fly', 'sail', 'go', 'move']
-
-    def func(self):
-        """ """
-        # cmd = self.cmdstring
         args = self.args.strip()
         char = self.character
-        # player = self.player
         where = self.obj
         # here = char.location
-        self.send_msg("%s%s|n commands in-operable %s%s|n vehicle to %s." %
-                      (char.STYLE, char.key, where.STYLE, where.key, args))
-        self.send_msg("%s%s|n does nothing." % (where.STYLE, where.key))
+        outside = where.location
+        player = self.player
+        if 'vehicle' in cmd:
+            player.msg('|wCommand list for %s%s|n:|/|C%s' % (where.STYLE, where.key, '|n, |C'.join(self.aliases)))
+        if 'board' in cmd or 'enter' in cmd:
+            entry_message = where.db.messages['entry']
+            char.msg('%s%s|n %s' % (char.STYLE, char.key, entry_message))
+            player.msg("You board %s." % where.get_display_name(player))
+            char.move_to(where)
+            self.send_msg('%s%s|n %s' % (char.STYLE, char.key, entry_message))
+        if 'disembark' in cmd or 'exit' in cmd or 'leave' in cmd:
+            exit_message = where.db.messages['exit']
+            char.msg('%s%s|n %s' % (char.STYLE, char.key, exit_message))
+            player.msg("You disembark %s." % where.get_display_name(player))
+            self.send_msg('%s%s|n %s' % (char.STYLE, char.key, exit_message))
+            char.move_to(outside)
+        if 'operate' in cmd:
+            self.send_msg("%s%s|n commands in-operable %s%s|n vehicle to %s." %
+                          (char.STYLE, char.key, where.STYLE, where.key, args))
+            self.send_msg("%s%s|n does nothing." % (where.STYLE, where.key))
