8f4a466e55e029622078f8f0b04e781274ab403f
AmberFennek <orkyporky@gmail.com>
2016-07-09 06:37:36 -0400

    Restructuring, error checking, and messages progress

--- NOWMU/NOW/commands/vehicle.py
+++ NOWMU/NOW/commands/vehicle.py
@@ -3,6 +3,8 @@ from evennia import default_cmds
 
 
 class VehicleCmdSet(CmdSet):
+    key = 'vehicle'
+
     def at_cmdset_creation(self):
         """Add command to the set - this set will be attached to the vehicle object (item or room)."""
         self.add(CmdVehicle())
@@ -16,12 +18,12 @@ class CmdVehicleDefault(default_cmds.MuxCommand):
     player_caller = True
 
     def send_msg(self, message):
-        """Send message internal and external to vehicle"""
+        """Send message internal and external to vehicle and optionally move vehicle."""
         char = self.character
         where = self.obj
         outside = where.location
         where.msg_contents(message, exclude=char)
-        outside.msg_contents(message)
+        outside.msg_contents(message, exclude=char)
 
 
 class CmdVehicle(CmdVehicleDefault):
@@ -38,24 +40,43 @@ class CmdVehicle(CmdVehicleDefault):
         args = self.args.strip()
         char = self.character
         where = self.obj
-        # here = char.location
+        here = char.location
         outside = where.location
         player = self.player
         if 'vehicle' in cmd:
             player.msg('|wCommand list for %s%s|n:|/|C%s' % (where.STYLE, where.key, '|n, |C'.join(self.aliases)))
         if 'board' in cmd or 'enter' in cmd:
-            entry_message = where.db.messages['entry']
-            char.msg('%s%s|n %s' % (char.STYLE, char.key, entry_message))
+            if here == where:
+                player.msg("You are already aboard %s." % where.get_display_name(player))
+                return
+            entry_message = None
+            if 'entry' in where.db.messages:
+                entry_message = where.db.messages['entry']
+            if entry_message:
+                char.msg('%s%s|n %s' % (char.STYLE, char.key, entry_message))
             player.msg("You board %s." % where.get_display_name(player))
+            if entry_message:
+                where.msg_contents('%s%s|n %s' % (char.STYLE, char.key, entry_message), exclude=char)
             char.move_to(where)
-            self.send_msg('%s%s|n %s' % (char.STYLE, char.key, entry_message))
+            if entry_message:
+                outside.msg_contents('%s%s|n %s' % (char.STYLE, char.key, entry_message))
         if 'disembark' in cmd or 'exit' in cmd or 'leave' in cmd:
-            exit_message = where.db.messages['exit']
-            char.msg('%s%s|n %s' % (char.STYLE, char.key, exit_message))
+            if here != where:
+                player.msg("You are not aboard %s." % where.get_display_name(player))
+                return
+            exit_message = None
+            if 'exit' in where.db.messages:
+                exit_message = where.db.messages['exit']
             player.msg("You disembark %s." % where.get_display_name(player))
-            self.send_msg('%s%s|n %s' % (char.STYLE, char.key, exit_message))
+            if exit_message:
+                self.send_msg('%s%s|n %s' % (char.STYLE, char.key, exit_message))
             char.move_to(outside)
+            if exit_message:
+                char.msg('%s%s|n %s' % (char.STYLE, char.key, exit_message))
         if 'operate' in cmd:
             self.send_msg("%s%s|n commands in-operable %s%s|n vehicle to %s." %
                           (char.STYLE, char.key, where.STYLE, where.key, args))
+            player.msg("%s%s|n commands in-operable %s%s|n vehicle to %s." %
+                       (char.STYLE, char.key, where.STYLE, where.key, args))
             self.send_msg("%s%s|n does nothing." % (where.STYLE, where.key))
+            player.msg("%s%s|n does nothing." % (where.STYLE, where.key))
