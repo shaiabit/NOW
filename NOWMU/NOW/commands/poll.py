24bdc544d2817d679672a4e54a28462e8a5e11b5
AmberFennek <orkyporky@gmail.com>
2016-07-09 06:38:20 -0400

    Restructuring, and some progress planning

--- NOWMU/NOW/commands/poll.py
+++ NOWMU/NOW/commands/poll.py
@@ -4,9 +4,12 @@ from evennia.utils.evmenu import get_input
 
 
 class PollCmdSet(CmdSet):
+    key = 'polling'
+
     def at_cmdset_creation(self):
         """Add command to the set - this set will be attached to the polling object (item or room)."""
         self.add(CmdPoll())
+        self.add(CmdSuggest())
 
 
 class CmdPoll(default_cmds.MuxCommand):
@@ -18,13 +21,10 @@ class CmdPoll(default_cmds.MuxCommand):
       vote   (Yet to be determined)
       quiz   (Yet to be determined)
       test   (Yet to be determined)
-      trivia   (Yet to be determined)
-         . . .
-      suggest [optional topic or brief suggestion (Up to 60 characters)]
-          (then prompted for optional long suggestion up to 300 characters)
+      trivia (Yet to be determined)
     """
     key = 'poll'
-    aliases = ['survey', 'vote', 'quiz', 'test', 'trivia', 'suggest']  # Support different types of question-asking.
+    aliases = ['survey', 'vote', 'quiz', 'test', 'trivia']  # Support different types of question-asking.
     locks = 'cmd:all()'
     arg_regex = r'\s|$'
     player_caller = True
@@ -37,27 +37,8 @@ class CmdPoll(default_cmds.MuxCommand):
         char = self.character
         here = char.location or 'the Void'
         player = self.player
-        args = self.args.strip()
         cmd = self.cmdstring
         where = self.obj
-
-        def suggest_callback(caller, prompt, user_input):
-            """"Response to input given after suggestion submitted"""
-            if len(user_input.strip()) < 1:
-                here.msg_contents("%s discards the full suggestion intended for %s." %
-                                  (char.get_display_name(player), where.get_display_name(player)))
-                player.msg("Long suggestion discarded. Only the topic or short suggestion was given to %s." %
-                           where.get_display_name(player))
-                return  # Abort the suggestion.
-            here.msg_contents("%s pops a suggestion into %s." %
-                              (char.get_display_name(player), where.get_display_name(player)))
-            # List structure to allow multiple suggestions to be saved. (By player and character name)
-            answer = [player.get_display_name(player), char.get_display_name(player), user_input[0:355].strip()]
-            if where.db.suggestions:
-                suggestions = list(where.db.suggestions)
-                where.db.suggestions = suggestions + answer
-            else:
-                where.db.suggestions = answer
 
         def look_ready(to):
             """Message/pose to the room: "looks ready to <insert message>. (adds ending period) """
@@ -81,20 +62,60 @@ class CmdPoll(default_cmds.MuxCommand):
         elif 'trivia' in cmd:
             look_ready("answer %s trivia" % where.get_display_name(player))
             player.msg("No trivia is available from %s." % where.get_display_name(player))
-        elif 'suggest' in cmd:
-            topic = args[0:80].strip()
-            if topic:
-                topic_answer = [player.get_display_name(player), char.get_display_name(player), topic]
-                if where.db.suggestions:
-                    suggestions1 = list(where.db.suggestions)
-                    where.db.suggestions = suggestions1 + topic_answer
-                else:
-                    where.db.suggestions = topic_answer
-                msg = '%s begins writing a suggestion for %s regarding "|w%s|n".' %\
-                      (char.get_display_name(player), where.get_display_name(player), topic)
+        pass
+
+
+class CmdSuggest(default_cmds.MuxCommand):
+    """
+    Poll player or characters with questions.
+    Usage:
+      suggest [optional topic or brief suggestion (Up to 60 characters)]
+          (then prompted for optional long suggestion up to 300 characters)
+    """
+    key = 'suggest'
+    locks = 'cmd:all()'
+    arg_regex = r'\s|$'
+    player_caller = True
+
+    def func(self):
+        """Allow player or character to submit short and/or long suggestions."""
+        char = self.character
+        here = char.location or 'the Void'
+        player = self.player
+        args = self.args.strip()
+        where = self.obj
+
+        def suggest_callback(caller, prompt, user_input):
+            """"Response to input given after suggestion submitted"""
+            if len(user_input.strip()) < 1:
+                here.msg_contents("%s%s|n discards the full suggestion intended for %s%s|n." %
+                                  (char.STYLE, char.key, where.STYLE, where.key))
+                player.msg("Long suggestion discarded. Only the topic or short suggestion was given to %s." %
+                           where.get_display_name(player))
+                return  # Abort the suggestion.
+            here.msg_contents("%s%s|n pops a suggestion into %s%s|n." %
+                              (char.STYLE, char.key, where.STYLE, where.key))
+            # List structure to allow multiple suggestions to be saved. (By player and character name)
+            answer = [player.get_display_name(player), char.get_display_name(player), user_input[0:355].strip()]
+            if where.db.suggestions:
+                suggestions = list(where.db.suggestions)
+                where.db.suggestions = suggestions + answer
             else:
-                msg = "%s begins writing a suggestion for %s regarding no specific topic." % \
-                      (char.get_display_name(player), where.get_display_name(player))
-            here.msg_contents(msg)
-            get_input(char, "|wSuggestion?|n (Type your suggestion now (300 characters or less),"
-                            " and then |g[enter]|n) ", suggest_callback)
+                where.db.suggestions = answer
+
+        topic = args[0:80].strip()
+        if topic:
+            topic_answer = [player.get_display_name(player), char.get_display_name(player), topic]
+            if where.db.suggestions:
+                suggestions1 = list(where.db.suggestions)
+                where.db.suggestions = suggestions1 + topic_answer
+            else:
+                where.db.suggestions = topic_answer
+            msg = '%s%s|n begins writing a suggestion for %s%s|n regarding "|w%s|n".' %\
+                  (char.STYLE, char.key, where.STYLE, where.key, topic)
+        else:
+            msg = "%s%s|n begins writing a suggestion for %s%s|n regarding no specific topic." % \
+                  (char.STYLE, char.key, where.STYLE, where.key)
+        here.msg_contents(msg)
+        get_input(char, "|wSuggestion?|n (Type your suggestion now (300 characters or less),"
+                        " and then |g[enter]|n) ", suggest_callback)
