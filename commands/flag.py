7b351e518eb9121dee13cb1cb7efa3540e93e92a
AmberFennek <orkyporky@gmail.com>
2016-07-12 17:12:37 -0400

    Flag command mock-up ready for abuse testing and feedback.

--- commands/flag.py
+++ commands/flag.py
@@ -9,28 +9,32 @@ class CmdFlag(default_cmds.MuxCommand):
       flag   shows flags on the current room.
       flag <object>  shows flags on that object.
       flag <object> = <flag>  sets flag on object.
-    Switches:
-    /search
-    /list  Shows a list of flags used.
+    Options:
+    /search  Search world for all objects with the given flag.
+    /list  Shows a list of flags in use in the world.
+    /info  Short information on given flags.
+    /long  Show more information about flags on given object.
     """
     key = 'flag'
     locks = 'cmd:all()'
     arg_regex = r'^/|\s|$'
-    help_category = 'System'
+    help_category = 'Building'
     player_caller = True
-    FLAGS = {'road': 'cars can drive into here, can take taxi from here',
+    FLAGS = {'street': 'cars can drive into here, can take taxi from here',
              'offroad': 'Same as "road" - but unpaved, slower speed',
-             'outside': 'weather occurs here, land and ascend to/from sky',
-             'weather': 'weather occurs here.Weather messages are custom per room.',
-             'shelter': 'weather observable here',
-             'water': 'there is water here, but also air',
-             'underwater': 'there is water, no way to surface for air',
+             'outside': 'Object can land and ascend to/from sky',
+             'weather': 'Custom weather occurs here, and can have effects.',
+             'shelter': 'weather is observable here, but does not have effect.',
+             'water': 'there is water here, but also air - ie. can breath.',
+             'aquatic': 'there is water, no way to surface for air',
              'air': 'flight into here is allowed by fliers',
              'cloud': 'denser air type, might allow alternate travel',
              'public': 'anyone allowed here',
              'disk': 'a stepdisk room, can jump to any other stepdisk from here.',
              'haunt': 'a room that allows dark characters.',
              'diet': 'room has food available. You can not starve here.',
+             'poll': 'a room or object that polls characters.',
+             'contain': 'an object that acts as a container.',
              'stealth': 'room does not announce when someone enters or leaves.'}
 
 #     myscript.tags.add("weather", category="climate")
@@ -44,9 +48,12 @@ class CmdFlag(default_cmds.MuxCommand):
         args = self.args.strip()
         lhs, rhs = [self.lhs, self.rhs]
         switches = self.switches
-        switches_list = [u'list', u'info', u'search']
+        switches_list = [u'list', u'info', u'long', u'search']
 
-        if not switches:
+        if not all(x in switches_list for x in switches):
+            player.msg("Not a valid switch for |y%s|n. Use only these: |g/%s" % (cmd, "|n, |g/".join(switches_list)))
+            return
+        if not switches or 'long' in switches:
             obj = here
             if not lhs:
                 if not here:
@@ -54,31 +61,53 @@ class CmdFlag(default_cmds.MuxCommand):
                     obj = char
             else:
                 obj = char.search(lhs, location=[char, char.location]) if args else here
+                if not obj:
+                    return
             flags_obj = obj.tags.all(category='flags')
             flags_obj_count = len(flags_obj)
             if flags_obj:  # returns a list of flags
-                player.msg('Flags in effect on %s: (%s) = |c%s' %
-                           (obj.get_display_name(player), flags_obj_count, "|n, |c".join(a for a in flags_obj)))
-        elif not all(x in switches_list for x in switches):
-            player.msg("Not a valid switch for %s. Use only these: |g/%s" % (cmd, "|n, |g/".join(switches_list)))
-            return
+                if 'long' in switches:
+                    player.msg('Flag list on %s: (%s): ' % (obj.get_display_name(player), flags_obj_count))
+                    for flag in flags_obj:
+                        player.msg('|c%s: |w%s|n|/' % (flag, self.FLAGS[flag]))
+                else:
+                    player.msg('Flag list on %s: (%s) = |c%s' %
+                               (obj.get_display_name(player), flags_obj_count, "|n, |c".join(a for a in flags_obj)))
+            else:
+                player.msg('Flag list is not found on %s.' % obj.get_display_name(player))
+            if rhs:  # If something is on the right hand side!
+                good_flag = [x for x in self.FLAGS if x in rhs]
+                overlap = [x for x in good_flag if x in flags_obj]
+                to_add = [x for x in good_flag if x not in overlap]
+                if overlap:
+                    is_one = abs(len(overlap)) == 1
+                    plural = '' if is_one else 's'
+                    player.msg('Flag%s already set: |y%s' % (plural, "|n, |y".join(a for a in overlap)))
+                is_one = abs(len(to_add)) == 1
+                plural = '' if is_one else 's'
+                player.msg('Added flag%s: |g%s' % (plural, "|n, |g".join(a for a in to_add)))
         if 'list' in switches:
             player.msg('Displaying list of ' + ('|ymatching' if args else '|gall') + '|n flags:')
             if args:  # Show list of matching flags
-                match_list = [x for x in self.FLAGS if x in args]
+                match_list = [x for x in self.FLAGS if x in args]  # This match is not "starts with" or partial.
                 player.msg('|c%s' % "|n, |c".join(match_list))
             else:
                 player.msg('|c%s' % "|n, |c".join(a for a in self.FLAGS))
         if 'info' in switches:
+            good_flag = [x for x in self.FLAGS if x in args] if args else self.FLAGS
             player.msg('Displaying info for ' + ('|ymatching' if args else '|gall') + '|n flags:')
-            player.msg('|c%s' % "|n, |c".join(a for a in self.FLAGS))
+            player.msg("|/".join('|c%s|n - |w%s' % (a, self.FLAGS[a]) for a in good_flag))
         if 'search' in switches:
             obj = search.search_tag(args, category='flags')
             object_count = len(obj)
             if object_count > 0:
                 match_string = ", ".join(o.get_display_name(player) for o in obj)
-                string = "Found |w%i|n object%s with flag '|g%s|n':\n %s" %\
-                         (object_count, "s" if object_count > 1 else '', args, match_string)
+                if args:
+                    string = "Found |w%i|n object%s with the flag '|g%s|n' set:\n %s" %\
+                             (object_count, "s" if object_count > 1 else '', args, match_string)
+                else:
+                    string = "Found |w%i|n object%s with any flag set:\n %s" % \
+                             (object_count, "s" if object_count > 1 else '', match_string)
                 player.msg(string)
             else:
                 player.msg('Found no objects flagged: "|g%s"' % args)
