5e34bdfa86d6e101b682b3bd0024dbcb48a64498
AmberFennek <orkyporky@gmail.com>
2016-07-26 00:21:42 -0400

    Add sweep option to home command.

--- commands/home.py
+++ commands/home.py
@@ -26,12 +26,13 @@ class CmdHome(default_cmds.MuxCommand):
         """Implement the command"""
         you = self.character
         player = self.player
+        opt = self.switches
         home = you.home
-        if not self.switches:
+        if not opt:
             if not home:
-                you.msg("You have no home yet.")
+                you.msg('You have no home yet.')
             elif home == you.location:
-                you.msg("You are already home!")
+                you.msg('You are already home!')
             else:
                 you.msg("There's no place like home ...")
                 you.move_to(home, use_destination=False)
@@ -42,20 +43,41 @@ class CmdHome(default_cmds.MuxCommand):
                 obj = you.search(self.lhs, global_search=True)
             if not obj:
                 return
-            if not self.rhs and 'here' not in self.switches:  # just view the destination set as home
+            if 'sweep' in opt:
+                home = obj.home
+                if not home:
+                    you.msg('%s has no home yet.' % obj.get_display_name(player))
+                elif home == obj.location:
+                    you.msg('%s is already home!' % obj.get_display_name(player))
+                elif obj != you and not player.check_permstring('Helpstaff') or not obj.access(player, 'puppet'):
+                    you.msg("You must have |wHelpstaff|n or higher access to send %s home."
+                            % obj.get_display_name(player))
+                else:
+                    obj.msg("There's no place like home ... (%s is sending you home.)" % you.get_display_name(player))
+                    if you.location:
+                        you.location.msg_contents('%s%s|n sends %s%s|n home.'
+                                                  % (you.STYLE, you.key, obj.STYLE, obj.key))
+                    was = obj.location
+                    obj.move_to(home, use_destination=False)
+                    if you.location != was:
+                        you.msg("%s left %s and went home to %s."
+                                % (obj.get_display_name(you), was.get_display_name(you), home.get_display_name(you)))
+                return
+            if not self.rhs and 'here' not in opt:  # just view the destination set as home
                 if obj != you and not player.check_permstring('Helpstaff') or not obj.access(player, 'puppet'):
-                    you.msg("You must have |wHelpstaff|n or higher access to view the home of %s." % obj)
+                    you.msg("You must have |wHelpstaff|n or higher access to view the home of %s."
+                            % obj.get_display_name(player))
                     return
                 home = obj.home
                 if not home:
                     string = "%s has no home location set!" % obj.get_display_name(you)
                 else:
-                    string = "%s's current home is %s." % (obj, home.get_display_name(you))
+                    string = "%s's current home is %s." % (obj.get_display_name(you), home.get_display_name(you))
             else:  # set/change a home location
                 if obj != you and not player.check_permstring('Mages') or not obj.access(player, 'puppet'):
                     you.msg("You must have |wMages|n or higher access to change the home of %s." % obj)
                     return
-                if self.rhs and 'here' not in self.switches:
+                if self.rhs and 'here' not in opt:
                     new_home = you.search(self.rhs, global_search=True)
                 else:
                     new_home = you.location
