c00b6394cd6ece7ef50ab2ca99e461404a61c676
AmberFennek <orkyporky@gmail.com>
2016-08-15 16:08:56 -0400

    mydie.py is Tria's project - updates are infrequent.

--- commands/mydie.py
+++ commands/mydie.py
@@ -57,6 +57,7 @@ class CmdMyDie(CmdMyDieDefault):
     /new - Create a new die with name set this die as current die to roll.
     /add - add a face to the die name or current die, if no name given.
     /del (or rem) - remove the last face [or named face] from the particular die.
+    /choose - Set the default die for your player
     /list (or show)  - show all die faces.
     /shuffle  - show all die faces in a shuffled order.
     /multi <n> - roll die n times.          (Roll, like dice - combinations )
@@ -69,13 +70,14 @@ class CmdMyDie(CmdMyDieDefault):
                    'coins': ['Quarter farthing', 'third farthing', 'half farthing', 'farthing', 'halfpenny', 'penny',
                              'threepence', 'groat', 'sixpense', 'shilling', 'florin', 'half crown', 'crown', 'pound',
                              'two pounds', 'five pounds'],
-                   'months': ['January', 'February', 'March', 'April', 'May', '...']}
+                   'months': ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September',
+                              'October', 'November', 'December']}
 
     def func(self):
         """ """
         cmd = self.cmdstring
         opt = self.switches
-        # args = self.args.strip()
+        args = self.args.strip()
         lhs, rhs = [self.lhs, self.rhs]
         char = self.character
         where = self.obj  # Where the dice rolling action is. (On the dice object)
@@ -83,41 +85,46 @@ class CmdMyDie(CmdMyDieDefault):
         # outside = where.location  # If you happen to be inside the dice, this is outside.
         player = self.player
         my_die = char.db.dice  # Check if character has any dice sets stored on it.
+        die = 'default'
         #  Does Die Exist?
-        if not my_die:  # Inform player how to add. Also suggest using help.
+        if not my_die:  # Inform player how to create. Also suggest using help.
             player.msg('You have no custom die yet. Use |y%s|g/new |w<|cdie name|w>|n to add a die.|/'
                        'See: |ghelp mydie|n for more information' % cmd)
-            return
-        else:
             dice = where.db.dice if where.db.dice else self.EXAMPLE_SET
-            current = dice[dice['default']]
-            # Is this Usage of Die or Modification of Die?
-            if not rhs:  # If no equals sign was supplied, then using dice sets, not modifying them.
-                # If using, randomize the die and store the result.
-                char.ndb.roll_result = random.shuffle(current)  # Store results on on character.
-                # Usage check for appropriate switches (ignore incorrect ones typos/new/add/rem <show)
-                # Generate roll result
-                result = []  # Start with an empty list.
-                if 'multi' in opt:
-                    pass  # TODO - get n results and append into result list.
-                if 'deal' in opt:
-                    pass  # TODO - draw n results from faces.
-                    #  (assuming n not > face_count, else repeats from next deck), append result.
-                else:  # A single roll.
-                    result = char.ndb.roll_result[0]
-                if 'list' in opt or 'show' in opt or 'shuffle' in opt:  # Shows all the die faces (Only to player?)
-                    face_count = len(char.ndb.roll_result)
-                    faces = '|c' + '|w, |c'.join(my_die if 'shuffle' in opt else char.db.dice)  # Potentially shuffle.
-                    player.msg('The current die, %s, has |g%i|n sides marked %s' % (current, face_count, faces))
-                # Next, send result to appropriate parties per options.
-                if 'secret' in opt or 'hidden' in opt:
-                    char.msg('You rolled %s to get %s.' % (current, result))
-                if 'hidden' in opt:  # Pose that character rolled current die, but do not display result.
-                    here.msg_contents('%s%s|n rolls a hidden %s die.' % (char.STYLE, char.key, current), exclude=char)
-                elif 'secret' not in opt:  # Pose roll and results.
-                    here.msg_contents('%s%s|n rolls the %s die from the %s%s|n to get %s' %
-                                      (char.STYLE, char.key, current, where.STYLE, where.key, result))
-            else:  # Modifying die
-                # Use switches to determine what to do, check for conflict no add & rem
-                # Inform use they have create die name they now need to /add faces
-                pass  # TODO - Need to design structure of dictionary before modifying.
+        else:
+            dice = my_die
+        if 'choose' in opt or 'set' in opt:
+            if args not in dice:
+                player.msg('That die (%s) does not exist. Choose another or create that die first before using.')
+                player.msg('Continuing to use die %s' % dice[die])
+        current = dice[dice[die]]
+        # Is this Usage of Die or Modification of Die?
+        if not rhs:  # If no equals sign was supplied, then using die, not modifying them.
+            # If using, randomize the die and store the result.
+            char.ndb.roll_result = random.shuffle(current)  # Store results on on character.
+            # Usage check for appropriate switches (ignore incorrect ones typos/new/add/rem <show)
+            # Generate roll result
+            result = []  # Start with an empty list.
+            if 'multi' in opt:
+                pass  # TODO - get n results and append into result list.
+            if 'deal' in opt:
+                pass  # TODO - draw n results from faces.
+                #  (assuming n not > face_count, else repeats from next deck), append result.
+            else:  # A single roll.
+                result = char.ndb.roll_result[0]
+            if 'list' in opt or 'show' in opt or 'shuffle' in opt:  # Shows all the die faces (Only to player?)
+                face_count = len(char.ndb.roll_result)
+                faces = '|c' + '|w, |c'.join(my_die if 'shuffle' in opt else char.db.dice)  # Potentially shuffle.
+                player.msg('The current die, %s, has |g%i|n sides marked %s' % (current, face_count, faces))
+            # Next, send result to appropriate parties per options.
+            if 'secret' in opt or 'hidden' in opt:
+                char.msg('You rolled %s to get %s.' % (current, result))
+            if 'hidden' in opt:  # Pose that character rolled current die, but do not display result.
+                here.msg_contents('%s%s|n rolls a hidden %s die.' % (char.STYLE, char.key, current), exclude=char)
+            elif 'secret' not in opt:  # Pose roll and results.
+                here.msg_contents('%s%s|n rolls the %s die from the %s%s|n to get %s' %
+                                  (char.STYLE, char.key, current, where.STYLE, where.key, result))
+        else:  # Modifying die
+            # Use switches to determine what to do, check for conflict no add & rem
+            # Inform use they have create die name they now need to /add faces
+            pass  # Design structure of dictionary is shown in EXAMPLE_SET
