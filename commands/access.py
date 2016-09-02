b33d8916b2357860c499571a8b13317cd44b6775
AmberFennek <orkyporky@gmail.com>
2016-09-02 00:59:49 -0400

    access.py +"quelled" indicator

--- commands/access.py
+++ commands/access.py
@@ -25,13 +25,15 @@ class CmdAccess(MuxCommand):
         if 'groups' in self.switches:
             string = "|wPermission Hierarchy|n (climbing): %s|/" % ", ".join(hierarchy_full)
         if player.is_superuser:
-            cperms = "<|ySuperuser|n> " + ", ".join(char.permissions.all())
             pperms = "<|ySuperuser|n> " + ", ".join(player.permissions.all())
+            cperms = "<|ySuperuser|n> " + ", ".join(char.permissions.all())
         else:
-            cperms = ", ".join(char.permissions.all())
             pperms = ", ".join(player.permissions.all())
+            cperms = ", ".join(char.permissions.all())
         string += "|wYour Player/Character access|n: "
         if hasattr(char, 'player'):
+            if char.player.attributes.has("_quell"):
+                string += "|r(quelled)|n "
             string += "Player: (%s: %s) and " % (player.get_display_name(self.session), pperms)
         string += "Character (%s: %s)" % (char.get_display_name(self.session), cperms)
         player.msg(string)
