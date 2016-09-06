e222baa5bfdb26e28536d24826faf6d3468f017f
W <tricuspa@netscape.net>
2016-09-06 14:24:14 -0700

    File obsolete, no longer in use.

--- world/locks.py
+++
@@ -1,9 +0,0 @@
-from random import *
-
-def half(accessing_obj, accessed_obj, *args, **kwargs):
-    if random() > 0.5:
-	return False
-    return True
-
-def roll(accessing_obj, accessed_obj, *args, **kwargs):
-    return True if args else False
