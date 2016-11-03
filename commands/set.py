6942fb6e8d4d5171764819bd70baaa5e3a564ab2
AmberFennek <orkyporky@gmail.com>
2016-11-03 01:26:10 -0400

    set.py show status and toggle on click.

--- commands/set.py
+++ commands/set.py
@@ -49,37 +49,44 @@ class CmdSettings(CmdSettingsDefault):
             if 'list' in opt or not args:
                 if not char.db.settings:
                     char.db.settings = {}
-                player.msg('Listing %s%s|n control panel settings: |g%s'
-                           % (char.STYLE, char.key, '|n, |g'.join('%s|n: |c%s' % (each, char.db.settings[each])
-                                                                  for each in char.db.settings)))
+                player.msg('Listing %s control panel settings: |g%s'
+                           % (char.get_display_name(player), '|n, |g'.join('|lcset/toggle %s|lt%s|le|n: |c%s'
+                                                                           % (each, each, char.db.settings[each])
+                                                                           for each in char.db.settings)))
                 return
             if 'on' in opt or 'off' in opt or 'toggle' in opt or 'symbol' in opt or 'message' in opt:
                 action = opt[0]
                 if action == 'on':
                     action = 'engage'
                     setting[args] = True
+                    status = True
                 elif action == 'off':
                     action = 'disengage'
                     setting[args] = False
+                    status = False
                 elif action == 'value':
                     action = 'dial'
                     setting[lhs] = int(rhs)
+                    status = int(rhs)
                 elif action == 'symbol':
                     action = 'dial'
                     setting[lhs] = rhs
+                    status = rhs
                 elif action == 'message':
                     action = 'set'
                     message[lhs] = rhs
+                    status = rhs
                 else:  # Toggle setting
                     setting[args] = False if char.db.settings and args in char.db.settings\
                                              and char.db.settings[args] else True
+                    status = setting[args]
                 mode = 'messages' if action == 'set' else 'settings'
                 if rhs:
-                    string = '|g%s|n %ss %s to %s on %s%s|n character %s.' % \
-                              (char.key, action, lhs if lhs else 'something', rhs, where.STYLE, where.key, mode)
+                    string = '|wYou %s %s to %s on %s character %s to "%s".' % \
+                              (action, lhs if lhs else 'something', rhs, char.get_display_name(player), mode, status)
                 else:
-                    string = '|g%s|n %ss %s on %s%s|n character %s.' %\
-                              (char.key, action, args if args else 'something', where.STYLE, where.key, mode)
+                    string = '|wYou %s %s on %s character %s to "%s".' %\
+                              (action, args if args else 'something', char.get_display_name(player), mode, status)
                 player.msg(string)  # Notify player of character setting changes.
                 if action == 'set' and rhs:
                     char.db.messages = message
