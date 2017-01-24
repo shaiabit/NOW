7d87bf027b121d8cb3ebf64504c13142964a2c5c
AmberFennek <orkyporky@gmail.com>
2017-01-24 17:49:29 -0500

    whisper.py Documentation and minor edits

--- commands/whisper.py
+++ commands/whisper.py
@@ -4,11 +4,34 @@ from commands.command import MuxCommand
 
 class CmdWhisper(MuxCommand):
     """
-    Whisper to a nearby character.
+    Whisper to one or more nearby targets.
+    Syntax:
+      whisper [target name, or names =[=] ][message]
     Usage:
-      whisper [character, or characters = ]<message>
-    Options:
+       whisper A, B, C = text of message
+    1) whisper text of message
+    2) whisper A, B, C =
+    3) whisper ==
+    4) whisper =
+    5) whisper
+    *1 Omitting the target(s) and equal sign will whisper
+       to the last whispered if message text is given.
+    *2 Supplying target(s), an equal sign, but no message
+      will set the target(s) for future whispers.
+    *3 Supplying two equal signs without target(s) or
+       message will set your next whisper to reply to
+       the last whispered group.
+    *4 Supplying only an equal sign without target(s) or
+       message text will set your next whisper to the one
+       who last whispered to you.
+    *5 If no parameters are given, whisper will display
+       the names of those whom you last whispered.
+    * In some environments, message degradation may occur,
+      similar to a mumble with only partial readability.
+    Planned Options:
     /o or /ooc  - Out-of-character whispering.
+    /t or /tel  - Telepathic whispering, if you and your
+                  target(s) have enough combined telepathy.
     """
     key = 'whisper'
     aliases = ['wh', '""', "''", 'say to']
@@ -20,8 +43,13 @@ class CmdWhisper(MuxCommand):
         who = self.lhs.strip()
         message = self.rhs.strip() if self.rhs else ''
         last = self.character.ndb.last_whispered
-        all_present = [each.key for each in self.character.location.contents]
+        all_present = [each.key for each in self.character.location.contents] +\
+                      [each.key for each in self.character.contents]
         result, new_last = self.whisper(who, message, last, all_present)
+        for each in new_last:
+            target = char.search(each, quiet=True)
+            if target and len(target) == 1:
+                target[0].private(char, 'whisper', message)
         char.msg(result)
         char.ndb.last_whispered = new_last
 
@@ -31,31 +59,32 @@ class CmdWhisper(MuxCommand):
         Whisper to one or more nearby targets.
         Syntax:
                whisper A, B, C = text of whisper
+               <command> <who> =[=] <message> (last, all_present)
           where:
-          self is me, the object that will whisper.
           who is a comma-delimited string of text from the left-hand
                side of the = sign, of who should receive this whisper.
                If left empty, the last whisperers list is used.
           message is the text from the right-hand side of the = sign,
                the message to be whispered.
-          last is a list of who last received a whisper from me,
-                used when who is empty.
+          last is a comma-delimited string (like who) of who last
+               received a whisper from me, used when who is empty.
           all_present is a list of which objects are near you, in range.
         """
+
+        # is_present, is_awake, is_unlocked, is_available.
 
         def is_present(object):  # Test if object is present in room
             return True if object in all_present else False
 
         def is_awake(object):  # To be altered after testing phase.
-            return True  # if object  == 'A' else False  # Simulates character A being awake.
+            return True  # Tests if character is awake.
 
         def convert_who_list(who_string):
             """
             This converts a comma-delimited string, returning
             a list with duplicates and whitespace removed.
             """
-            converted_who = [each_who.strip() for each_who in who_string.split(',')]
-            return list(set(converted_who))
+            return list(set([each_who.strip() for each_who in who_string.split(',')])) if who_string else []
 
         who_present, who_success, failed_whisper = [], [], []
         whisper_list = convert_who_list(who if who else last)
@@ -64,15 +93,15 @@ class CmdWhisper(MuxCommand):
             if is_present(each):
                 who_present.append(each)
             else:
-                failed_whisper.append(each + " (not in room)")
+                failed_whisper.append(each + "(not in room)")
         for each in who_present:
             if is_awake(each):
                 who_success.append(each)
                 whisper_success = True
             else:
-                failed_whisper.append(each + " (not awake)")
+                failed_whisper.append(each + "(not awake)")
         if whisper_success:
-            result_message = 'You whispered ' + message + ' to ' + ', '.join(who_success) + '.'
+            result_message = 'You whispered "' + message + '" to ' + ', '.join(who_success) + '.'
         else:
             result_message = 'The names provided were unable to be whispered to.'
         if len(failed_whisper) > 0:
