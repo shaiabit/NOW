947b544b439802b98674448484302e46cb140ff5
AmberFennek <orkyporky@gmail.com>
2017-01-24 19:52:15 -0500

    inventory.py - mxp_name depreciated
    Use get_display_name's mxp kwarg instead!  It's actually making the code less complicated.

--- commands/inventory.py
+++ commands/inventory.py
@@ -33,9 +33,8 @@ class CmdInventory(MuxCommand):
                 i_mass = mass_unit(item.get_mass()) if hasattr(item, 'get_mass') else 0
                 second = '(|y%s|n) ' % i_mass if 'weight' in self.switches else ''
                 second += item.db.desc_brief or item.db.desc or ''
-                table.add_row('%s' % item.mxp_name(you.sessions, 'sense #%s' % item.id)
-                              if hasattr(item, 'mxp_name')
-                              else item.get_display_name(you.sessions), second or '')
+                table.add_row('%s' % item.get_display_name(you.sessions, mxp=('sense %s' % item.key)),
+                              second or '')
             my_mass, my_total_mass = [mass, you.get_mass() if hasattr(you, 'get_mass') else 0]
             string = "|wYou (%s) and what you carry (%s) total |y%s|n:\n%s" %\
                      (mass_unit(mass), mass_unit(my_total_mass - my_mass),
