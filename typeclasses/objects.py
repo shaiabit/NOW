ac412d4ef38fda1d8d87c5457c6be6e8e41f8803
AmberFennek <orkyporky@gmail.com>
2017-04-11 06:30:41 -0400

    Implicitly call add

--- typeclasses/objects.py
+++ typeclasses/objects.py
@@ -328,7 +328,7 @@ class Object(Tangible):
     def get_mass(self):  # TODO: Add mass of objects on surface, also.
         if not self.traits.mass:
             mass = 1 if not self.db.mass else self.db.mass
-            self.traits.add('mass', 'Mass', type='static', base=mass)
+            self.traits.add('mass', 'Mass', 'static', mass)
         mass = self.traits.mass.actual or 1
         return reduce(lambda x, y: x+y.get_mass(), [mass] + self.contents)
 
