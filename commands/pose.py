e123969abafa4a3ba3ce7f23dc7f851dcbfec68f
AmberFennek <orkyporky@gmail.com>
2017-04-27 03:23:36 -0400

    Added allowing room posing non-character items
     Cleanup of Pose code, added set_doing method

--- commands/pose.py
+++ commands/pose.py
@@ -1,16 +1,19 @@
 # -*- coding: UTF-8 -*-
 from commands.command import MuxCommand
 from world.helpers import escape_braces
+from evennia.utils import ansi
 
 
 class CmdPose(MuxCommand):
     """
     Describe and/or attempt to trigger an action on an object.
     The pose text will automatically begin with your name.
-    pose, emote, :, ;
+    pose, :, ;, do, pp
     Usage:
       pose <pose text>
       pose's <pose text>
+      do <pose>
+      do <target>=<pose>
       pp <pose text> = <verb> <noun>
     Options:
     /o or /ooc  (Out-of-character to the room or channel.)
@@ -19,11 +22,15 @@ class CmdPose(MuxCommand):
       Additional options for do, rp, doing command version of pose:
       /reset   (Reset the current room pose to use default setting)
       /default (Set the default room pose for all rooms, all times)
-      /quiet or silent (Update doing, but do not pose) 
-    
+      /quiet or /silent (Update doing, but do not pose)
+
     Example:
       > pose is standing by the tree, smiling.
       Rulan is standing by the tree, smiling.
+      > do is standing by the tree, smiling.
+      Current room pose set to "Rulan is standing by the tree, smiling."
+      > do stapler=is out of staples.
+      Current room pose set to "Orange Swingline stapler is out of staples."
     """
     # > ppose strains to lift the anvil. = get anvil
     # > Werewolf strains to lift the anvil.
@@ -34,70 +41,95 @@ class CmdPose(MuxCommand):
     locks = 'cmd:all()'
     player_caller = True
 
+    def set_doing(self, setter, pose, target=None, default=False):
+        """
+        
+        Args:
+            self:  (Cmd)
+            setter: (Tangible) object setting pose
+            pose: (str) pose to set on object
+            target: (Tangible) object to pose
+            default: (bool) Setting default
+
+        Returns:
+            pose, pose_default
+        """
+        if not target:
+            target = setter
+        if not target.db.messages:
+            target.db.messages = dict(pose='', pose_default='')
+        if target.db.messages.get('pose') is None:
+            target.db.messages['pose'] = ''
+        if target.db.messages.get('pose_default') is None:
+            target.db.messages['pose_default'] = ''
+        if not setter:
+            return target.db.messages['pose'], target.db.messages['pose_default']
+        if not target.access(setter, 'edit') and setter is not target:  # target being accessed/edited by setter
+            setter.msg('You have no permission to edit %s.' % target.get_display_name(setter))
+            return None
+        if not pose:
+            setter.msg('Wot?')
+            return
+        set_this = 'pose' if not default else 'pose_default'
+        target.db.messages[set_this] = pose
+        return target.db.messages['pose'], target.db.messages['pose_default']
+
     def func(self):
-        """Basic pose"""
+        """Basic pose, power pose, room posing - all in one"""
         cmd = self.cmdstring
         opt = self.switches
         args = unicode(self.args).strip()
+        lhs, rhs = self.lhs, self.rhs
         char = self.character
         player = self.player
         here = char.location if char else None
         non_space_chars = ['®', '©', '°', '·', '~', '@', '-', "'", '’', ',', ';', ':', '.', '?', '!', '…']
-        magnet = True if args and args[0] in non_space_chars or cmd == ";" else False
+        raw_pose = rhs if rhs else args
+        magnet = True if raw_pose and raw_pose[0] in non_space_chars or cmd == ";" else False
         power = True if self.cmdstring == 'ppose' or self.cmdstring == 'pp' or self.cmdstring == 'p:' else False
-        pose = ('' if magnet else '|_') + (self.lhs if power and self.rhs else args)
-        if 'do' in cmd or 'rp' in cmd:
-            target = char
+        doing = True if 'do' in cmd or 'rp' in cmd else False
+        pose = ('' if magnet else '|_') + (ansi.strip_ansi(raw_pose) if doing else raw_pose)
+        # ---- Setting Room poses as a doing message ----------
+        if doing:  # Pose will have no markup when posing on the room, to minimize shenanigans.
+            target = char  # Initially assume the setting character is the target.
             if not args and 'reset' not in opt:
                 has_pose = char.db.messages and char.db.messages.get('pose')
-                if has_pose:
-                    char.msg("Current pose reads: '%s'" % char.get_display_name(char, pose=True))
+                if has_pose:  # If target has poses set, display them to the setter.
+                    char.msg("Current pose reads: '%s'" % target.get_display_name(char, pose=True))
                     default_pose = target.db.messages and target.db.messages.get('pose_default') or None
                     if default_pose:
                         char.msg('Default pose is \'%s%s\'' % (char.get_display_name(char), default_pose))
                     else:
                         char.msg('Default pose not set.')
                 else:
-                    char.msg('No pose has been set.|/Usage: rp <pose-text> OR pose obj = <pose-text>')
+                    char.msg('No pose has been set.|/Usage: rp <pose-text> OR pose <obj> = <pose-text>')
                 return
-            if target and '=' in self.args:  # affect something else other than self.
-                target = char.search(target)
+            if len(pose) > 60:  # Pose length in characters, not counting the poser's name.
+                char.msg('Your pose is too long.')
+                return
+            if rhs:  # pose something else other than self.
+                target = char.search(lhs)  # Search for a reference to the target.
                 if not target:
                     return
-                if not target.access(char, 'edit'):
-                    char.msg('You have no permission to edit %s.' % target.get_display_name(char))
-                    return
-                pass  # TODO: Apply setting to target.
-            else:
+                if pose:
+                    self.set_doing(char, pose, target)  # Try to set the pose of the target.
+            else:  # pose self.
                 target = char
-            if not char.db.messages:
-                char.db.messages = {}
-            char.db.messages['pose'] = pose
-            char.db.messages['pose_default'] = '|_is here.'  # set Default if so
-            target_name = target.sdesc.get() if hasattr(target, '_sdesc') else target.key
-            # reset the pose to default
-            if 'reset' in opt:
+            if 'reset' in opt:  # Clears current temp doing, reverts it to default.
                 pose = target.db.messages and target.db.messages.get('pose_default', '')
                 if not target.db.messages:
                     target.db.messages = {}
                 target.db.messages['pose'] = pose
-            elif 'default' in opt:
-                if not target.db.messages:
-                    target.db.messages = {}
-                target.db.messages['pose_default'] = pose
-                char.msg("Default pose is now: '%s%s'" % (char.get_display_name(char), pose))
-                return
-            else:
-                if len(target_name) + len(pose) > 60:
-                    char.msg('Your pose is too long.')
-                    return
-                if not target.db.messages:
-                    target.db.messages = {}
-                target.db.messages['pose'] = pose
-                if self.args and not ('silent' in opt or 'quiet' in opt):
-                    player.execute_cmd(';%s' % pose)
-            char.msg("Pose now set to: '%s'" % char.get_display_name(char, pose=True))
-        else:  # Action pose, not static Room Pose.
+            elif 'default' in opt:  # Sets doing pose default.
+                self.set_doing(char, pose, target, True)  # True means "set default", not temp doing.
+                char.msg("Default pose is now: '%s%s'" % (target.get_display_name(char), pose))
+                return  # Nothing more to do. Default never poses to room, just sets doing message.
+            elif not rhs:
+                self.set_doing(char, pose)  # Setting temp doing message on the setter...
+                if args and not ('silent' in opt or 'quiet' in opt) and char is target:  # Allow set without posing
+                    player.execute_cmd(';%s' % pose)  # pose to the room like a normal pose would.
+            char.msg("Pose now set to: '%s'" % target.get_display_name(char, pose=True))  # Display name with pose.
+        else:  # ---- Action pose, not static Room Pose. ---------------------
             if 'magnet' in opt or 'm' in opt:
                 char.msg("Pose magnet glyphs are %s." % non_space_chars)
             if not (here and char):
