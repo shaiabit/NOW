2b42126f65116d91ef265863a30d65ba19db2c63
AmberFennek <orkyporky@gmail.com>
2017-06-05 18:08:17 -0400

    CmdQuit notifies all sessions differently
    Only gives exit_msg to the session(s) that are being disconnected.
    Gives disconnection message notice to all sessions.

--- commands/quit.py
+++ commands/quit.py
@@ -23,27 +23,32 @@ class CmdQuit(MuxPlayerCommand):
         player = self.player
         bye = '|RDisconnecting|n'
         exit_msg = 'Hope to see you again, soon.'
-        reason = self.args.strip()
-
+        reason = self.args.strip() if self.args else 'Quitting'
         if reason:
-            bye += " ( |w%s ) " % self.args.strip()
-
+            bye += " ( |w%s ) " % reason
         if 'all' in self.switches:
             msg = bye + ' all sessions. ' + exit_msg
-            player.msg(msg, session=self.session)
             for session in player.sessions.all():
+                player.msg(msg, session=session)
+                player.msg(exit_msg, session=session)
                 player.disconnect_session_from_player(session, reason=reason)
         else:
             session_count = len(player.sessions.all())
             if session_count == 2:
-                msg = bye + '. One session is still connected.'
-                player.msg(msg, session=self.session)
+                msg = bye
+                others = self.player.sessions.get()
+                if self.session in others:
+                    others.remove(self.session)
+                print(repr(all))
+                player.msg(msg + 'One session remains connected.', session=self.session)
+                player.msg(msg + 'Your session remains connected.', session=others)
             elif session_count > 2:
-                msg = bye + ". %i sessions are still connected."
-                player.msg(msg % (session_count - 1), session=self.session)
+                msg = bye + "%i sessions are still connected."
+                player.msg(msg % (session_count - 1))
             else:
                 # If quitting the last available session, give connect time.
                 online = utils.time_format(time.time() - self.session.conn_time, 1)
                 msg = bye + ' after ' + online + ' online. ' + exit_msg
                 player.msg(msg, session=self.session)
+            player.msg(exit_msg, session=self.session)
             player.disconnect_session_from_player(self.session, reason=reason)
