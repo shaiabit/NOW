8dd38ba6712cb09fb250c4d5c8c7fc173a54b9ba
AmberFennek <orkyporky@gmail.com>
2017-07-03 12:09:08 -0400

    Updating CmdWhisper version

--- commands/whisper.py
+++ commands/whisper.py
@@ -34,36 +34,39 @@ class CmdWhisper(MuxCommand):
                   target(s) have enough combined telepathy.
     """
     key = 'whisper'
-    aliases = ['wh', '""', "''", 'say to']
+    aliases = ['wh', '""', "''"]
+    options = ('version',)
     locks = 'cmd:all()'
 
     def func(self):
         """Run the whisper command"""
         char = self.character
+        here = char.location
         who = self.lhs.strip()
         opt = self.switches
         message = self.args.strip() if not self.rhs else self.rhs.strip()  # If no equals sign, use args for message
         last = self.character.ndb.last_whispered or []
-        all_present = [each.key for each in self.character.location.contents] +\
-                      [each.key for each in self.character.contents]
-        result, new_last = self.whisper(who, message, last, all_present)
+        all_present = [each for each in self.character.location.contents] +\
+                      [each for each in self.character.contents] + [here]
+        result, new_last = self.whisper(char, who, message, last, all_present)
         for each in new_last:
-            target = char.search(each, quiet=True)
-            if target and len(target) == 1:
-                target[0].private(char, 'whisper', message)
+            if each:
+                each.private(char, 'whisper', message)
         char.msg(result)
         char.ndb.last_whispered = new_last
-        if 'ver' in opt:
-            char.msg('Whisper version 14, Tuesday 31 Jan 2017')
+        if 'version' in opt:
+            # char.msg('Whisper version 14, Tuesday 31 Jan 2017')
+            char.msg('Whisper version https://repl.it/FDZS/23')
 
     @staticmethod
-    def whisper(who, message, last, all_present):
+    def whisper(me, who, message, last, all_present):
         """
         Whisper to one or more nearby targets.
         Syntax:
                whisper A, B, C = text of whisper
                <command> <who> =[=] <message> (last, all_present)
           where:
+          me is the object that is whispering.
           who is a comma-delimited string of text from the left-hand
                side of the = sign, of who should receive this whisper.
                If left empty, the last whisperers list is used.
@@ -82,45 +85,42 @@ class CmdWhisper(MuxCommand):
         def is_unlocked(obj):  # Test if object is present in room
             return True
 
-        def is_present(obj):  # Test if object is present in room
-            return True if obj in all_present else False
-
         def is_awake(obj):  # To be altered after testing phase.
             return True  # Tests if character is awake.
 
-        def convert_who_list(who_string):
-            """
-            This converts a comma-delimited string, returning
-            a list with duplicates and whitespace removed.
-            """
-            return list(set([each_who.strip() for each_who in who_string.split(',')]))
+        def match_object(each, all_present):
+            return me.search(each, candidates=all_present)
 
+        # Begin whisper code for Whisper command here.
         who_present, who_success, failed_whisper = [], [], []
-        whisper_list = convert_who_list(who) if who else last
+        whisper_list = list(set([each_who.strip() for each_who in who.split(',')])) if who else None
+        object_list = [match_object(each, all_present) for each in whisper_list] if who else last
         whisper_success = False
+        result_message = ''
 
-        for each in whisper_list:
-            if is_available(each):
-                if is_unlocked(each):
-                    if is_present(each):
-                        if is_awake(each):
-                            who_success.append(each)
-                            whisper_success = True
-                        else:
-                            who_success.append(each)
-                            whisper_success = True
-                            failed_whisper.append(each + " (is asleep)")
+        for obj in object_list:
+            if not obj:
+                continue  # Skip objects not found.
+            if is_available(obj):
+                if is_unlocked(obj):
+                    if is_awake(obj):
+                        who_success.append(obj)
+                        whisper_success = True
                     else:
-                        failed_whisper.append(each + " (is not present)")
+                        who_success.append(obj)
+                        whisper_success = True
+                        failed_whisper.append(obj.name + " (is asleep)")
                 else:
-                    failed_whisper.append(each + " (is uninterested)")
+                    failed_whisper.append(obj.name + " (is uninterested)")
             else:
-                failed_whisper.append(each + " (is not available/unreachable)")
+                failed_whisper.append(obj.name + " (is not available/unreachable)")
 
         if whisper_success:
-            result_message = 'You whispered "' + message + '" to ' + ', '.join(who_success) + '.'
-        else:
-            result_message = 'The names provided were unable to be whispered to.'
-        if len(failed_whisper) > 0:
+            result_message = 'You whisper "' + message + '" to ' + ', '.join([obj.name for obj in who_success]) + '.'
+        if failed_whisper:
+            plural, verb = ['', 'was'] if (len(failed_whisper) == 1) else ['s', 'were']
+            result_message += '\nThe name%s provided %s unable to be whispered to.' % (plural, verb)
             result_message += '\n  Your whisper was not heard by ' + ', '.join(failed_whisper) + '.'
         return result_message, who_success  # String displayed to self showing whisper results.
+
+        # End whisper code for Whisper command here.
