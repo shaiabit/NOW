7b550c44ac18203343b255b591926f0a6cb39636
AmberFennek <orkyporky@gmail.com>
2017-09-25 15:08:08 -0400

    0.7 conversion

--- commands/quit.py
+++ commands/quit.py
@@ -1,10 +1,10 @@
 # -*- coding: UTF-8 -*-
-from commands.command import MuxPlayerCommand
+from commands.command import MuxAccountCommand
 from evennia.utils import utils
 import time
 
 
-class CmdQuit(MuxPlayerCommand):
+class CmdQuit(MuxAccountCommand):
     """
     Gracefully disconnect your current session and send optional
     quit reason message to your other sessions, if any.
@@ -21,33 +21,33 @@ class CmdQuit(MuxPlayerCommand):
 
     def func(self):
         """hook function"""
-        player = self.player
+        account = self.account
         bye = '|RDisconnecting|n'
         exit_msg = 'Hope to see you again, soon.'
         reason = self.args.strip() if self.args else 'Quitting'
         if reason:
             bye += " ( |w%s|n ) " % reason
         if 'all' in self.switches:
-            for session in player.sessions.all():
+            for session in account.sessions.all():
                 session_online_time = utils.time_format(time.time() - session.conn_time, 1)
                 msg = bye + ' all sessions after ' + session_online_time + ' online. '
-                player.msg(msg, session=session)
-                player.msg(exit_msg, session=session)
-                player.disconnect_session_from_player(session, reason=reason)
+                account.msg(msg, session=session)
+                account.msg(exit_msg, session=session)
+                account.disconnect_session_from_account(session, reason=reason)
         else:
-            session_count = len(player.sessions.all())
+            session_count = len(account.sessions.all())
             online = utils.time_format(time.time() - self.session.conn_time, 1)
             if session_count == 2:
                 msg = bye
-                others = [x for x in self.player.sessions.get() if x is not self.session]
-                player.msg(msg + 'after ' + online + ' online.', session=self.session)
-                player.msg(msg + 'your other session. |gThis session remains connected.|n', session=others)
+                others = [x for x in self.account.sessions.get() if x is not self.session]
+                account.msg(msg + 'after ' + online + ' online.', session=self.session)
+                account.msg(msg + 'your other session. |gThis session remains connected.|n', session=others)
             elif session_count > 2:
                 msg = bye + "%i sessions are still connected."
-                player.msg(msg % (session_count - 1))
+                account.msg(msg % (session_count - 1))
             else:
                 # If quitting the last available session, give connect time.
                 msg = bye + 'after ' + online + ' online. '
-                player.msg(msg, session=self.session)
-            player.msg(exit_msg, session=self.session)
-            player.disconnect_session_from_player(self.session, reason=reason)
+                account.msg(msg, session=self.session)
+            account.msg(exit_msg, session=self.session)
+            account.disconnect_session_from_account(self.session, reason=reason)
