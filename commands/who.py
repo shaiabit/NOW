dad5ef2594c4a8031c787586c15fb90e5ce52acf
AmberFennek <orkyporky@gmail.com>
2017-09-25 16:36:54 -0400

    0.7 conversion

--- commands/who.py
+++ commands/who.py
@@ -1,11 +1,11 @@
 # -*- coding: UTF-8 -*-
 import time
-from commands.command import MuxPlayerCommand
+from commands.command import MuxAccountCommand
 from evennia.server.sessionhandler import SESSIONS
 from evennia.utils import ansi, utils, create, search, evtable
 
 
-class CmdWho(MuxPlayerCommand):
+class CmdWho(MuxAccountCommand):
     """
     Shows who is currently online.
     Usage:
@@ -20,12 +20,12 @@ class CmdWho(MuxPlayerCommand):
     locks = 'cmd:all()'
 
     def func(self):
-        """Get all connected players by polling session."""
-        you = self.player
+        """Get all connected accounts by polling session."""
+        you = self.account
         session_list = SESSIONS.get_sessions()
         cmd = self.cmdstring
         show_session_data = you.check_permstring('Immortals') and not you.attributes.has('_quell')
-        player_count = (SESSIONS.player_count())
+        account_count = (SESSIONS.account_count())
         table = evtable.EvTable(border='none', pad_width=0, border_width=0, maxwidth=79)
         if cmd == 'wa' or cmd == 'where':
             # Example output expected:
@@ -67,15 +67,13 @@ class CmdWho(MuxPlayerCommand):
             table.reformat_column(1, width=8, align='l')
             table.reformat_column(2, width=7, pad_right=1, align='r')
             for element in my_character.location.contents:
-                if not element.has_player:
+                if not element.has_account:
                     continue
                 delta_cmd = time.time() - max([each.cmd_last_visible for each in element.sessions.all()])
                 delta_con = time.time() - min([each.conn_time for each in element.sessions.all()])
                 name = element.get_display_name(you)
-                type = element.db.messages and element.db.messages.get('species') or ''
-                gend = element.db.messages and element.db.messages.get('gender') or ''
-                fill = ' ' if gend else ''
-                table.add_row(name + ', ' + gend.lower() + fill + type if type else name,
+                type = element.attributes.get('species', default='')
+                table.add_row(name + ', ' + type if type else name,
                               utils.time_format(delta_con, 0), utils.time_format(delta_cmd, 1))
         elif cmd == 'what' or cmd == 'wot':
             table.add_header('|wCharacter  - Doing', '|wIdle')
@@ -90,22 +88,22 @@ class CmdWho(MuxPlayerCommand):
                 table.add_row(doing, utils.time_format(delta_cmd, 1))
         else:  # Default to displaying who
             if show_session_data:  # privileged info shown to Immortals and higher only when not quelled
-                table.add_header('|wCharacter', '|wPlayer', '|wQuell', '|wCmds', '|wProtocol', '|wAddress')
+                table.add_header('|wCharacter', '|wAccount', '|wQuell', '|wCmds', '|wProtocol', '|wAddress')
                 table.reformat_column(0, align='l')
                 table.reformat_column(1, align='r')
                 table.reformat_column(2, width=7, align='r')
                 table.reformat_column(3, width=6, pad_right=1, align='r')
                 table.reformat_column(4, width=11, align='l')
                 table.reformat_column(5, width=16, align='r')
-                session_list = sorted(session_list, key=lambda o: o.player.key)
+                session_list = sorted(session_list, key=lambda o: o.account.key)
                 for session in session_list:
                     if not session.logged_in:
                         continue
-                    player = session.get_player()
+                    account = session.get_account()
                     puppet = session.get_puppet()
                     table.add_row(puppet.get_display_name(you) if puppet else 'None',
-                                  player.get_display_name(you),
-                                  '|gYes|n' if player.attributes.get('_quell') else '|rNo|n',
+                                  account.get_display_name(you),
+                                  '|gYes|n' if account.attributes.get('_quell') else '|rNo|n',
                                   session.cmd_total, session.protocol_key,
                                   isinstance(session.address, tuple) and session.address[0] or session.address)
             else:  # unprivileged info shown to everyone, including Immortals and higher when quelled
@@ -123,8 +121,8 @@ class CmdWho(MuxPlayerCommand):
                         continue
                     table.add_row(character.get_display_name(you), utils.time_format(delta_conn, 0),
                                   utils.time_format(delta_cmd, 1))
-        is_one = player_count == 1
-        string = '%s' % 'A' if is_one else str(player_count)
+        is_one = account_count == 1
+        string = '%s' % 'A' if is_one else str(account_count)
         string += ' single ' if is_one else ' unique '
         plural = ' is' if is_one else 's are'
         string += 'account%s logged in.' % plural
