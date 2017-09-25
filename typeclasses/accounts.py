97583eaccb002f1453862516fd74bfd56c9bec7a
AmberFennek <orkyporky@gmail.com>
2017-09-25 13:52:19 -0400

    0.7 conversion

--- typeclasses/accounts.py
+++ typeclasses/accounts.py
@@ -1,16 +1,16 @@
 """
-Player
+Account
 
-The Player represents the game "account" and each login has only one
-Player object. A Player is what chats on default channels but has no
-other in-game-world existance. Rather the Player puppets Objects (such
+The Account represents the game "account" and each login has only one
+Account object. An Account is what chats on default channels but has no
+other in-game-world existence. Rather the Account puppets Objects (such
 as Characters) in order to actually participate in the game world.
 
 
 Guest
 
-Guest players are simple low-level accounts that are created/deleted
-on the fly and allows users to test the game without the committment
+Guest accounts are simple low-level accounts that are created/deleted
+on the fly and allows users to test the game without the commitment
 of a full registration. Guest accounts are deactivated by default; to
 activate them, add the following line to your settings file:
 
@@ -20,12 +20,12 @@ You will also need to modify the connection screen to reflect the
 possibility to connect with a guest account. The setting file accepts
 several more options for customizing the Guest account system.
 """
-from evennia import DefaultPlayer, DefaultGuest
+from evennia import DefaultAccount, DefaultGuest
 
 
-class Player(DefaultPlayer):
+class Account(DefaultAccount):
     """
-    This class describes the actual OOC player (i.e. the user connecting
+    This class describes the actual OOC account (i.e. the user connecting
     to the MUD). It does NOT have visual appearance in the game world (that
     is handled by the character which is connected to this). Comm channels
     are attended/joined using this object.
@@ -34,12 +34,12 @@ class Player(DefaultPlayer):
     should generally not hold any character-related info (that's best handled
     on the character level).
 
-    Can be set using BASE_PLAYER_TYPECLASS.
+    Can be set using BASE_ACCOUNT_TYPECLASS.
 
 
     * available properties
 
-     key (string) - name of player
+     key (string) - name of account
      name (string)- wrapper for user.username
      aliases (list of strings) - aliases to the object.
               Will be saved to database as AliasDB entries but returned as strings.
@@ -48,8 +48,8 @@ class Player(DefaultPlayer):
      permissions (list of strings) - list of permission strings
 
      user (User, read-only) - django User authorization object
-     obj (Object) - game object controlled by player. 'character' can also be used.
-     sessions (list of Sessions) - sessions connected to this player
+     obj (Object) - game object controlled by account. 'character' can also be used.
+     sessions (list of Sessions) - sessions connected to this account
      is_superuser (bool, read-only) - if the connected user is a superuser
 
     * Handlers
@@ -67,7 +67,7 @@ class Player(DefaultPlayer):
      swap_character(new_character, delete_old_character=False)
      execute_cmd(raw_string, session=None)
      search(ostring, global_search=False, attribute_name=None, use_nicks=False,
-            location=None, ignore_errors=False, player=False)
+            location=None, ignore_errors=False, account=False)
      is_typeclass(typeclass, exact=False)
      swap_typeclass(new_typeclass, clean_attributes=False, no_default=True)
      access(accessing_obj, access_type='read', default=False)
@@ -76,7 +76,7 @@ class Player(DefaultPlayer):
     * Hook methods (when re-implementation, remember methods need to have self as first arg)
 
      basetype_setup()
-     at_player_creation()
+     at_account_creation()
 
      - note that the following hooks are also found on Objects and are
        usually handled on the character level:
@@ -133,7 +133,7 @@ class Player(DefaultPlayer):
             session.execute_cmd('@ic')
 
     def at_disconnect(self):
-        super(Player, self).at_disconnect()
+        super(Account, self).at_disconnect()
         # sessions = self.sessions.all()
         # session = sessions[-1]
         # do_not_exceed = 24  # Keep the last dozen entries
@@ -148,7 +148,7 @@ class Player(DefaultPlayer):
 
 class Guest(DefaultGuest):
     """
-    This class is used for guest logins. Unlike Players, Guests and their
+    This class is used for guest logins. Unlike Accounts, Guests and their
     characters are deleted after disconnection.
     """
     STYLE = '|[305'
