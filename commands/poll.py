8195efc79289fa3301a4fe883233cf34d46521fe
AmberFennek <orkyporky@gmail.com>
2017-09-25 14:26:56 -0400

    0.7 conversion

--- commands/poll.py
+++ commands/poll.py
@@ -15,7 +15,7 @@ class PollCmdSet(CmdSet):
 
 class CmdPoll(MuxCommand):
     """
-    Poll player or characters with questions.
+    Poll account or characters with questions.
     Usage:
       poll   (Yet to be determined)
       survey (Yet to be determined)
@@ -27,46 +27,46 @@ class CmdPoll(MuxCommand):
     aliases = ['survey', 'vote', 'test', 'trivia']  # Support different types of question-asking.
     locks = 'cmd:all()'
     arg_regex = r'\s|$'
-    player_caller = True
+    account_caller = True
 
 # list_of_polls = [{'name': 'A sample poll', 'questions': [['Do you?', 'yes', 'no', 'maybe'], 'Would you?',  ...
     # ['Can you?', ['yes', 'no', 'sometimes', 'always']]]}]
 
     def func(self):
-        """Poll player or characters with questions."""
+        """Poll account or characters with questions."""
         char = self.character
         here = char.location or 'the Void'
-        player = self.player
+        account = self.account
         cmd = self.cmdstring
         where = self.obj
 
         def look_ready(to):
             """Message/pose to the room: "looks ready to <insert message>. (adds ending period) """
-            here.msg_contents("%s looks ready to %s." % (char.get_display_name(player), to))
+            here.msg_contents("%s looks ready to %s." % (char.get_display_name(account), to))
 
         if 'poll' in cmd:
-            look_ready("take a %s poll" % where.get_display_name(player))
-            player.msg("No polls available from %s." % where.get_display_name(player))
+            look_ready("take a %s poll" % where.get_display_name(account))
+            account.msg("No polls available from %s." % where.get_display_name(account))
         elif 'survey' in cmd:
-            look_ready("take a survey on %s" % where.get_display_name(player))
-            player.msg("No surveys are available from %s." % where.get_display_name(player))
+            look_ready("take a survey on %s" % where.get_display_name(account))
+            account.msg("No surveys are available from %s." % where.get_display_name(account))
         elif 'vote' in cmd:
-            look_ready("vote with %s" % where.get_display_name(player))
-            player.msg("No voting issues are available from %s." % where.get_display_name(player))
+            look_ready("vote with %s" % where.get_display_name(account))
+            account.msg("No voting issues are available from %s." % where.get_display_name(account))
         elif 'quiz' in cmd:
-            look_ready("take a %s quiz" % where.get_display_name(player))
-            player.msg("No quizes are available from %s." % where.get_display_name(player))
+            look_ready("take a %s quiz" % where.get_display_name(account))
+            account.msg("No quizes are available from %s." % where.get_display_name(account))
         elif 'test' in cmd:
-            look_ready("take a %s test" % where.get_display_name(player))
-            player.msg("No tests are available from %s." % where.get_display_name(player))
+            look_ready("take a %s test" % where.get_display_name(account))
+            account.msg("No tests are available from %s." % where.get_display_name(account))
         elif 'trivia' in cmd:
-            look_ready("answer %s trivia" % where.get_display_name(player))
-            player.msg("No trivia is available from %s." % where.get_display_name(player))
+            look_ready("answer %s trivia" % where.get_display_name(account))
+            account.msg("No trivia is available from %s." % where.get_display_name(account))
 
 
 class CmdSuggest(MuxCommand):
     """
-    Poll player or characters with questions.
+    Poll account or characters with questions.
     Usage:
       suggest [optional topic or brief suggestion (Up to 60 characters)]
           (then prompted for optional long suggestion up to 300 characters)
@@ -74,13 +74,13 @@ class CmdSuggest(MuxCommand):
     key = 'suggest'
     locks = 'cmd:all()'
     arg_regex = r'\s|$'
-    player_caller = True
+    account_caller = True
 
     def func(self):
-        """Allow player or character to submit short and/or long suggestions."""
+        """Allow account or character to submit short and/or long suggestions."""
         char = self.character
         here = char.location or 'the Void'
-        player = self.player
+        account = self.account
         args = self.args.strip()
         where = self.obj
 
@@ -89,13 +89,13 @@ class CmdSuggest(MuxCommand):
             if len(user_input.strip()) < 1:
                 here.msg_contents("%s%s|n discards the full suggestion intended for %s%s|n." %
                                   (char.STYLE, char.key, where.STYLE, where.key))
-                player.msg("Long suggestion discarded. Only the topic or short suggestion was given to %s." %
-                           where.get_display_name(player))
+                account.msg("Long suggestion discarded. Only the topic or short suggestion was given to %s." %
+                           where.get_display_name(account))
                 return  # Abort the suggestion.
             here.msg_contents("%s%s|n pops a suggestion into %s%s|n." %
                               (char.STYLE, char.key, where.STYLE, where.key))
-            # List structure to allow multiple suggestions to be saved. (By player and character name)
-            answer = [player.get_display_name(player), char.get_display_name(player), user_input[0:355].strip()]
+            # List structure to allow multiple suggestions to be saved. (By account and character name)
+            answer = [account.get_display_name(account), char.get_display_name(account), user_input[0:355].strip()]
             if where.db.suggestions:
                 suggestions = list(where.db.suggestions)
                 where.db.suggestions = suggestions + answer
@@ -104,7 +104,7 @@ class CmdSuggest(MuxCommand):
 
         topic = args[0:80].strip()
         if topic:
-            topic_answer = [player.get_display_name(player), char.get_display_name(player), topic]
+            topic_answer = [account.get_display_name(account), char.get_display_name(account), topic]
             if where.db.suggestions:
                 suggestions1 = list(where.db.suggestions)
                 where.db.suggestions = suggestions1 + topic_answer
@@ -122,17 +122,17 @@ class CmdSuggest(MuxCommand):
 
 class CmdQuiz(MuxCommand):
     """
-    Poll player or characters with questions.
+    Poll account or characters with questions.
     Usage:
       quiz   (Yet to be determined)
     """
     key = 'quiz'
     locks = 'cmd:all()'
     arg_regex = r'\s|$'
-    player_caller = True
+    account_caller = True
 
     def func(self):
-        """Allow player or character to submit short and/or long suggestions."""
+        """Allow account or character to submit short and/or long suggestions."""
         char = self.character
         EvMenu(char, 'commands.poll', startnode='quiz_start', cmd_on_exit=None, persistent=False)
 
