3fbd20a582611ec0383991087bc5f05897b10903
AmberFennek <orkyporky@gmail.com>
2017-09-25 20:41:38 -0400

    0.7 conversion

--- commands/zone.py
+++ commands/zone.py
@@ -15,7 +15,7 @@ class CmdZone(MuxCommand):
     key = 'zone'
     locks = 'cmd:all()'
     help_category = 'System'
-    player_caller = True
+    account_caller = True
 
     # myscript.tags.add('weather', category='climate')
     # mychair.tags.all(category='flags')  # returns a list of Tags
@@ -26,7 +26,7 @@ class CmdZone(MuxCommand):
         """ """
         char = self.character
         here = char.location
-        player = self.player
+        account = self.account
         cmd = self.cmdstring
         switches = self.switches
         args = self.args.strip()
@@ -39,16 +39,16 @@ class CmdZone(MuxCommand):
                 zones_here.append(here.tags.get(category='region'))
                 zones_here.append(here.tags.get(category='area'))
                 zones_here.append(here.tags.get(category='zone'))
-                player.msg('Zone here: |c%s' % "|n, |c".join(a for a in [x for x in zones_here if x is not None]))
+                account.msg('Zone here: |c%s' % "|n, |c".join(a for a in [x for x in zones_here if x is not None]))
             else:
-                player.msg("No realm zone found here. You are not in any realm.")
+                account.msg("No realm zone found here. You are not in any realm.")
                 return
         elif not all(x in switches_list for x in switches):
-            player.msg("Not a valid switch for %s. Use only these: |g/%s" % (cmd, "|n, |g/".join(switches_list)))
+            account.msg("Not a valid switch for %s. Use only these: |g/%s" % (cmd, "|n, |g/".join(switches_list)))
             return
         if 'search' in switches:
             if not args or args not in zones or self.lhs not in zones:
-                player.msg("Searching requires providing a search string.  Try one of: zone, area, region, or realm.")
+                account.msg("Searching requires providing a search string.  Try one of: zone, area, region, or realm.")
                 return
             else:
                 category = self.rhs or args
@@ -56,9 +56,9 @@ class CmdZone(MuxCommand):
                 rooms = search.search_tag(zone, category=category)
                 room_count = len(rooms)
                 if room_count > 0:
-                    match_string = ", ".join(r.get_display_name(player) for r in rooms)
+                    match_string = ", ".join(r.get_display_name(account) for r in rooms)
                     string = "Found |w%i|n room%s with zone category '|g%s|n':\n %s" % \
                              (room_count, "s" if room_count > 1 else '', args, match_string)
-                    player.msg(string)
+                    account.msg(string)
                 else:
-                    player.msg("No %s zones found." % args)
+                    account.msg("No %s zones found." % args)
