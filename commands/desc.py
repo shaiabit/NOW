e18e543652839c14803a84776404c5ca9a01f64d
AmberFennek <orkyporky@gmail.com>
2017-10-01 15:31:43 -0400

    +/room option to CmdDesc
    Includes checking permission to edit

--- commands/desc.py
+++ commands/desc.py
@@ -25,29 +25,31 @@ def _desc_quit(caller):
 
 class CmdDesc(MuxCommand):
     """
-    Describe yourself!
+    Describe yourself (or your room)
     Usage:
       desc[/option] [description]
     Options:
     /edit  - Use a line-based editor similar to vi for more advanced editing.
     /brief - Add a brief description, 65 characters or less.
     /side - Adds a side description (inside container, outside of room)
+    /room - Adds a room description (update desc of current room)
 
     Add a description to your character, visible to characters who look at you.
     """
     key = 'desc'
-    options = ('edit', 'brief', 'side')
+    options = ('edit', 'brief', 'side', 'room')
     locks = 'cmd:all()'
     arg_regex = r'^/|\s|$'
 
     def edit_handler(self):
         char = self.character
+        opt = self.switches
         if self.args:
             self.msg("|yYou may specify a description, or use the edit switch, "
                      "but not both.|n")
             return
         else:
-            obj = char
+            obj = char.location if 'room' in opt else char
 
         def load(obj):
             return obj.db.evmenu_target.db.desc or ''
@@ -72,11 +74,16 @@ class CmdDesc(MuxCommand):
     def func(self):
         """Add the description."""
         char = self.character
-        if 'edit' in self.switches:
-            self.edit_handler()
+        opt = self.switches
+        target = here if 'room' in opt else char
+        if 'edit' in opt:
+            if target.access(char, 'edit'):
+                self.edit_handler()
+            else:
+                self.caller.msg("You don't have access to edit {}.".format(target.get_display_name(char)))
             return
         if not self.args:
-            if 'brief' in self.switches:
+            if 'brief' in opt:
                 self.caller.msg("You must supply a brief description (65 characters or less) to describe yourself.")
                 return
 
@@ -89,19 +96,22 @@ class CmdDesc(MuxCommand):
                 contents = caller.location.contents
                 for obj in contents:
                     obj.msg(msg)
-                if 'side' in self.switches:
+                if 'side' in opt:
                     caller.db.desc_side = user_input.strip()
                 else:
                     caller.db.desc = user_input.strip()
 
             get_input(char, "Type your description now, and then |g[enter]|n: ", desc_callback)
             return
-        if 'brief' in self.switches:
+        if 'brief' in opt:
             self.caller.db.desc_brief = self.args[0:65].strip()
             self.caller.msg("Your brief description has been saved: %s" % self.caller.db.desc_brief)
             return
-        if 'side' in self.switches:
+        if 'side' in opt:
             char.db.desc_side = self.args
         else:
-            char.db.desc = self.args
-        char.msg('You successfully set your description.')
+            if not target.access(char, 'edit'):
+                self.caller.msg("You don't have access to edit {}.".format(target.get_display_name(char)))
+                return
+            target.db.desc = self.args
+        char.msg('You successfully described {}.format(target.get_display_name(char))')
