c6f47c4c02cbdac5236e79490a640bc6e11b410d
AmberFennek <orkyporky@gmail.com>
2017-10-20 01:38:08 -0400

    CmdRoll defaults and output formatting

--- commands/mydie.py
+++ commands/mydie.py
@@ -136,7 +136,7 @@ class CmdRoll(CmdMyDieDefault):
     """
     Usage:
       roll [xdy]
-    x number of y-sided dice
+    x number of y-sided dice, defaults to rolling a single d6
     """
     key = 'roll'
     locks = 'cmd:all()'
@@ -148,14 +148,20 @@ class CmdRoll(CmdMyDieDefault):
         """
         Rolls xdy: x number of y-sided dice.
         """
-        try:
-            lhs = int(self.lhs)
-        except ValueError:
-            lhs = 1  # Force default of rolling 1 die if user-provided input fails
-        try:
-            rhs = int(self.rhs)
-        except ValueError:
-            rhs = 6  # Force default of 6-sided dice if user-provided input fails
+        if self.lhs:
+            try:
+                lhs = int(self.lhs)
+            except ValueError:
+                lhs = 1  # Force default of rolling 1 die if user-provided input fails
+        else:
+            lhs = 1
+        if self.rhs:
+            try:
+                rhs = int(self.rhs)
+            except ValueError:
+                rhs = 6  # Force default of 6-sided dice if user-provided input fails
+        else:
+            rhs = 6
         result = self.roll_dice(lhs, rhs, return_tuple=True)
         if not result:
             self.msg('Roll: Number of sides and number of dice must be greater than zero.')
@@ -164,5 +170,9 @@ class CmdRoll(CmdMyDieDefault):
         rolls = result[3]
         rolling = len(rolls)
         rollers = 'dice' if rolling > 1 else 'die'
-        self.msg('{0} imaginary {1}-sided {2} roll{3} {4} for a total of {5}.'.format(
-            rolling, rhs, rollers, '' if rolling > 1 else 's', repr(rolls), total))
+        if rolling > 1:
+            self.msg('{0} imaginary {1}-sided {2} roll {3} for a total of {4}.'.format(
+                rolling, rhs, rollers, repr(rolls), total))
+        else:
+            self.msg('{0} imaginary {1}-sided {2} rolls {3}.'.format(
+                rolling, rhs, rollers, total))
