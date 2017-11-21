77c07652d5ab59370d22a82897aa392c7cb42ff8
AmberFennek <orkyporky@gmail.com>
2017-11-21 13:43:04 -0500

    CmdPortal now sets locks on portal entry
    Entry locks depend on which command alias and switches were used.

--- commands/portal.py
+++ commands/portal.py
@@ -11,23 +11,20 @@ class CmdPortal(MuxCommand):
     The portal forms after a slight delay and is, under some circumstances,
     one-way only.
     Usage:
-      portal/switch <object>[ = <location>]
+      portal/option <target>
     Options:
-    /quiet     Portal arrives quietly to others in room.
-    /only      Only the summoned object can use the portal exit.
-    /in        Portal is one-way; into summon location.
-    /out       Portal is one-way; summon location outward
-    /vanish    if set, the portal leads to Nothingness.
-               When this option is used, <location> is ignored.
+    /quiet     Portal arrives quietly. Only you and target are notified.
+    /only      Only the involved parties can use the portal.
+    /in        Portal is one-way; into your location.
+    /out       Portal is one-way; out from your location.
     Examples:
       summon/only Tria
       join Rulan
       meet/quiet LazyLion
-      portal/vanish me
     """
     key = 'portal'
     aliases = ['meet', 'join', 'summon']
-    options = ('quiet', 'only', 'in', 'out', 'vanish')
+    options = ('quiet', 'silent', 'only', 'in', 'out', 'vanish')
     locks = 'cmd:pperm(denizen)'
     help_category = 'Travel'
 
@@ -45,21 +42,21 @@ class CmdPortal(MuxCommand):
         account = self.account
         args = self.args
         lhs, rhs = self.lhs, self.rhs
-        opt = self.switches  # TODO - add code to make the switches work.
+        opt = self.switches
         
-        error_private = ' in a private room that does not allow portals to form.'
+        message_private = ' in a private room that does not allow portals to form.'
 
         if char and char.ndb.currently_moving:
-            account.msg("You can not open a portal while moving. (|rstop|n, then try again.)")
+            account.msg("You can not open a portal while moving. (|lcstop|lt|rStop|n|le, then try again.)")
             return
-        if not args:
-            char.msg("Could not find target. Usage: {} <character or NPC>".format(cmd))
+        if not args and 'vanish' not in opt:
+            char.msg('Usage: {} <character or NPC>'.format(cmd))
             return
         session_list = SESSIONS.get_sessions()
         target = []
         # Check for private flag on source room. It must be controlled by summoner if private.
         if loc.tags.get('private', category='flags') and not loc.access(char, 'control'):
-            char.msg('You are' + error_private)
+            char.msg('You are' + message_private)
             return
         # Check object pool filtered by tagged "pool" and located in None.
         obj_pool = [each for each in evennia.search_tag('pool', category='portal') if not each.location]
@@ -80,44 +77,57 @@ class CmdPortal(MuxCommand):
         elif len(target) > 1:  # Too many partial matches, try exact matching.
             char.msg("Unique character name not found.")
             return
-        else:
-            target = target[0]
-            # Check for private flag on destination room. If so, check for in/out locks.
-            there = target.location
-            if there and there.tags.get('private', category='flags') and not there.access(char, 'control'):
-                char.msg('Destination of portal is' + error_private)
-                return
+        target = target[0]
+        # Check for private flag on destination room. If so, check for in/out locks.
+        there = target.location
+        if there and there.tags.get('private', category='flags') and not there.access(char, 'control'):
+            char.msg('Destination of portal is' + message_private)
+            return
         # Check if A can walk to B, or B to A depending on meet or summon
-            meet_message = 'You are being joined by {summoner} from {loc}.'
-            summon_message = 'You are being summoned to {loc} by {summoner}.'
-            message = meet_message if 'meet' in cmd else summon_message
-            loc_name = loc.get_display_name(target)
-            target_name = target.get_display_name(char)
-            char_name = char.get_display_name(target)
-            target.msg(message.format(summoner=char_name, loc=loc_name))
-            target.msg('A portal should appear soon.')
-            char.msg("You begin to open a portal connecting %s" % target_name +
-                     " and your location.")
+        join_message = 'You are being invited to meet {summoner} in {loc}.'
+        meet_message = 'You are being joined by {summoner} from {loc}.'
+        summon_message = 'You are being summoned to {loc} by {summoner}.'
+        message = meet_message if 'meet' in cmd else (summon_message if 'summon' in cmd else join_message)
+        loc_name = loc.get_display_name(target)
+        target_name = target.get_display_name(char)
+        char_name = char.get_display_name(target)
+        target.msg(message.format(summoner=char_name, loc=loc_name))
+        target.msg('A portal should appear soon.')
+        char.msg("You begin to open a portal connecting %s" % target_name + " and your location.")
 
         def open_portal():
             """Move inflatable portals into place."""
-            portal_enter.move_to(target.location)
-            portal_exit.move_to(loc)
-            # If only in opt, lock entering to the target, only
-            # If in or out, meet or summon, lock one portal end, depending.
-            # If portal to Nothingness, only send one portal
+            # If in or out, join or summon, lock portals, depending.
+            enter_lock, exit_lock = 'all()', 'all()'
+            if 'only' in opt:
+                enter_lock = 'id({}) OR id({})'.format(target.id, char.id)
+                exit_lock = 'id({}) OR id({})'.format(target.id, char.id)
+            if 'in' in opt or 'join' in cmd:
+                enter_lock = 'none()'
+            if 'out' in opt or 'summon' in cmd:
+                exit_lock = 'none()'
+            portal_enter.locks.add('enter:' + enter_lock)
+            portal_exit.locks.add('enter:' + exit_lock)
+            quiet = True if ('quiet' in opt or 'silent' in opt) else False
+            portal_enter.move_to(target.location, quiet=quiet)
+            if quiet:
+                target.msg('{} quietly appears in {}.'.format(portal_enter.get_display_name(target), loc_name))
+                char.msg('{} quietly appears in {}.'.format(portal_exit.get_display_name(char),
+                                                             loc.get_display_name(char)))
+            portal_exit.move_to(loc, quiet=quiet)
 
         delay(10, callback=open_portal)  # 10 seconds later, the portal (exit pair) appears.
 
         def close_portal():
             """Remove and store inflatable portals in Nothingness."""
-            for each in portal_enter.contents:
-                each.move_to(target.location)
-            for each in portal_exit.contents:
-                each.move_to(loc)
+            for every in portal_enter.contents:
+                every.move_to(target.location)
+            for every in portal_exit.contents:
+                every.move_to(loc)
+            # if not quiet:
             portal_enter.location.msg_contents("|r%s|n vanishes into |xNo|=gth|=fin|=egn|=des|=css|n." % portal_enter)
             portal_exit.location.msg_contents("|r%s|n vanishes into |xNo|=gth|=fin|=egn|=des|=css|n." % portal_exit)
-            portal_enter.move_to(None, to_none=True)
-            portal_exit.move_to(None, to_none=True)
+            portal_enter.move_to(None, to_none=True)  # , quiet=quiet)
+            portal_exit.move_to(None, to_none=True)  # , quiet=quiet)
 
         delay(180, callback=close_portal)  # Wait, then move portal objects to the portal pool in Nothingness
