8f181baffe351430d08f511bd0398a3e2c3a493d
AmberFennek <orkyporky@gmail.com>
2018-01-15 04:14:26 -0500

    CmdAccess groups option moved to hierarchy alias

--- commands/access.py
+++ commands/access.py
@@ -5,17 +5,14 @@ from commands.command import MuxCommand
 
 class CmdAccess(MuxCommand):
     """
-    Displays your current world access levels for
-    your current account and character account.
     Usage:
-      access[/option]
-    Options:
-    /groups  - Also displays the system's permission groups hierarchy.
+      access      -  Display your account and character access level.
+      hierarchy   -  Displays the system's permission groups hierarchy.
     """
     key = 'access'
-    options = ('groups',)
+    aliases = ['hierarchy']
     locks = 'cmd:all()'
-    help_category = 'System'
+    help_category = 'Information'
     account_caller = True
 
     def func(self):
@@ -24,19 +21,20 @@ class CmdAccess(MuxCommand):
         account = self.account
         hierarchy_full = settings.PERMISSION_HIERARCHY
         info = []  # List of info to output to user
-        if 'groups' in self.switches:
+        if 'hierarchy' in self.cmdstring:
             info.append('|wPermission Hierarchy|n (climbing): %s|/' % ", ".join(hierarchy_full))
-        if account.is_superuser:
-            pperms = '<|ySuperuser|n> ' + ', '.join(account.permissions.all())
-            cperms = ('<|ySuperuser|n> ' + ', '.join(char.permissions.all())) if char else None
         else:
-            pperms = ', '.join(account.permissions.all())
-            cperms = (', '.join(char.permissions.all())) if char else None
-        info.append('|wYour Account' + ('/Character' if char else '') + ' access|n: ')
-        if account:
-            if account.attributes.has('_quell'):
-                info.append('|r(quelled)|n ')
-            info.append('Account: (%s: %s)' % (account.get_display_name(account), pperms))
-        if cperms:
-            info.append(' and Character (%s: %s)' % (char.get_display_name(char), cperms))
+            if account.is_superuser:
+                pperms = '<|ySuperuser|n> ' + ', '.join(account.permissions.all())
+                cperms = ('<|ySuperuser|n> ' + ', '.join(char.permissions.all())) if char else None
+            else:
+                pperms = ', '.join(account.permissions.all())
+                cperms = (', '.join(char.permissions.all())) if char else None
+            info.append('|wYour Account' + ('/Character' if char else '') + ' access|n: ')
+            if account:
+                if account.attributes.has('_quell'):
+                    info.append('|r(quelled)|n ')
+                info.append('Account: (%s: %s)' % (account.get_display_name(account), pperms))
+            if cperms:
+                info.append(' and Character (%s: %s)' % (char.get_display_name(char), cperms))
         self.msg(''.join(info))
